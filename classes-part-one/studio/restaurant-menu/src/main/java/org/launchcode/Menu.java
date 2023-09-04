677681b0e595eaca9fd38998ddffa86fd76c5ce4
collinaustin <collinaustin@gmail.com>
2023-09-04 15:43:20 -0500

    Finished Chapter 3 studio.

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -3,7 +3,27 @@ import java.util.*;
 
 public class Menu {
 
+    private ArrayList<MenuItem> items;
+    private Date dateUpdated;
 
+    public Menu(ArrayList<MenuItem> items, Date dateUpdated) {
+        this.items = items;
+        this.dateUpdated = dateUpdated;
+    }
 
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
 
+    public void setItems(ArrayList<MenuItem> items) {
+        this.items = items;
+    }
+
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -14,16 +14,32 @@ public class MenuItem {
         this.isNew = isNew;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public boolean isNew() {
+        return isNew;
     }
 
     public void setNew(boolean aNew) {
