fbbbef7d8b6e6abb14523745378a6ea3b81261a9
collinaustin <collinaustin@gmail.com>
2023-09-14 13:24:22 -0500

    Finished Chapter 6 examples.

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -6,8 +6,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBracketsTest {
     //TODO: add tests here
+
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void openBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void openBracketsReturnsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void openBracketsReturnsFalse3() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[L[aunchCode"));
+    }
+
+    @Test
+    public void noBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void openBracketsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[], ][, [][]"));
+    }
+
+    @Test
+    public void closeBracketsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void closeBracketsFalseOneBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+
+
+
 }
